182f26847050c33c017282aa21ce8158755d5d79
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-01-13 19:33:18 -0500

    update train script

--- train.sh
+++ train.sh
@@ -5,11 +5,11 @@ docker build -f Dockerfile.deps -t cropmask/dependencies .
 
 export AWS_CREDENTIALS=<PATH TO AWS CREDENTIALS FILE> # ie.$HOME/.aws/credentials
 export CLEARML_CREDENTIALS=<PATH TO CLEARML CREDENTIALS FILE> # ie. $HOME/clearml.conf
-export DATASETS=<DATASETS> #ie. "all"
-export MODEL_NAME=<MODEL NAME> # ie. "kenya"
+export DATASETS=<DATASETS> #ie. "geowiki_landcover_2017, kenya_non_crop, one_acre_fund_kenya, plant_village_kenya"
+export MODEL_NAME=<MODEL NAME> # ie. kenya
 
 printf "Once model begins training, you can view progress here:\n\
-https://app.community.clear.ml/projects/15e0af16e2954760b5acf7d0117d4cdc"
+https://app.community.clear.ml/projects/15e0af16e2954760b5acf7d0117d4cdc\n"
 
 # Trains model
 docker build -f Dockerfile.train \
