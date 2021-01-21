4eabdd124bfe7611327e6558311a9aa4b101bd95
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-01-21 06:59:34 -0800

    use temp files for all unit tests

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import sys
 import os
+from typing import Optional
 
 sys.path.append("..")
 
@@ -10,7 +11,12 @@ from src.models import Model
 from src.analysis import plot_results
 
 
-def make_prediction(model: Model, test_path: Path, save_dir: Path, with_forecaster: bool = True):
+def make_prediction(
+        model: Model,
+        test_path: Path,
+        save_dir: Path,
+        with_forecaster: bool = False,
+        plot_results_enabled: bool = False) -> Optional[Path]:
     if with_forecaster:
         prefix = "forecasted_"
     else:
@@ -22,17 +28,19 @@ def make_prediction(model: Model, test_path: Path, save_dir: Path, with_forecast
         return
 
     out = model.predict(test_path, with_forecaster=with_forecaster)
-    plot_results(out, test_path, savepath=save_dir, prefix=prefix)
+    if plot_results_enabled:
+        plot_results(out, test_path, savepath=save_dir, prefix=prefix)
     out.to_netcdf(file_path)
     return file_path
 
 
-def gdal_merge(save_dir: Path):
+def gdal_merge(save_dir: Path) -> Path:
     file_list = save_dir.glob('*.nc')
     files_string = " ".join([str(file) for file in file_list])
     merged_file = save_dir / "merged.tif"
     command = f"gdal_merge.py -o {merged_file} -of gtiff {files_string}"
     os.system(command)
+    return merged_file
 
 
 def main(path_to_tif_files: str, model_name: str, merge_predictions: bool = False, data_dir: str = "../data"):
@@ -47,8 +55,8 @@ def main(path_to_tif_files: str, model_name: str, merge_predictions: bool = Fals
 
     for test_path in test_files:
         print(f"Running for {test_path}")
-        make_prediction(model, test_path, save_dir, with_forecaster=True)
-        make_prediction(model, test_path, save_dir, with_forecaster=False)
+        make_prediction(model, test_path, save_dir, with_forecaster=True, plot_results_enabled=False)
+        make_prediction(model, test_path, save_dir, with_forecaster=False, plot_results_enabled=False)
 
     if merge_predictions:
         gdal_merge(save_dir)
--- tests/scripts/test_predict.py
+++ tests/scripts/test_predict.py
@@ -1,51 +1,94 @@
 from unittest import TestCase
 from unittest.mock import patch
 from pathlib import Path
-from scripts.predict import make_prediction
 import xarray as xr
+import gdal
+import osr
+import tempfile
+import shutil
+from scripts.predict import make_prediction
+
+
+def generate_empty_tif(file_path: Path) -> Path:
+    driver = gdal.GetDriverByName('GTiff')
+    spatref = osr.SpatialReference()
+    spatref.ImportFromEPSG(27700)
+    wkt = spatref.ExportToWkt()
+    num_bands = 13
+    nodata = 255
+    xres = 5
+    yres = -5
+    xmin = 0
+    xmax = 5
+    ymin = 0
+    ymax = 5
+    dtype = gdal.GDT_Int16
+
+    xsize = abs(int((xmax - xmin) / xres))
+    ysize = abs(int((ymax - ymin) / yres))
+
+    ds = driver.Create(str(file_path), xsize, ysize, num_bands, dtype, options=['COMPRESS=LZW', 'TILED=YES'])
+    ds.SetProjection(wkt)
+    ds.SetGeoTransform([xmin, xres, 0, ymax, 0, yres])
+    ds.GetRasterBand(1).Fill(0)
+    ds.GetRasterBand(1).SetNoDataValue(nodata)
+    ds.FlushCache()
+    return file_path
+
 
 class TestPredict(TestCase):
 
+    @classmethod
+    def setUpClass(cls):
+        cls.temp_dir = Path(tempfile.mkdtemp())
+        cls.empty_tif = generate_empty_tif(cls.temp_dir / 'empty.tif')
+        cls.forecasted_path = cls.temp_dir / f'preds_forecasted_{cls.empty_tif.name}.nc'
+        cls.normal_path = cls.temp_dir / f'preds_normal_{cls.empty_tif.name}.nc'
+
+    @classmethod
+    def tearDownClass(cls):
+        shutil.rmtree(cls.temp_dir)
+
     @patch('src.models.Model')
     @patch('scripts.predict.plot_results')
-    def test_make_prediction(self, mock_model, mock_plot_results):
-        # Setup
+    def test_make_prediction_normal(self, mock_plot_results, mock_model):
         mock_model.predict.return_value = xr.Dataset(None)
-        mock_plot_results.return_value = None
-        save_dir = Path('../data/predictions')
+        output_path = make_prediction(model=mock_model,
+                                      test_path=self.empty_tif,
+                                      save_dir=self.temp_dir)
+        mock_model.predict.assert_called()
+        mock_plot_results.assert_not_called()
+        self.assertEqual(output_path, self.normal_path)
+        self.assertTrue(output_path.exists())
+        output_path.unlink()
 
-        tif_file_name = 'empty.tif'
-        test_path = Path(f'../data/tif_files/{tif_file_name}')
-        expected_forecasted_path = save_dir / f'preds_forecasted_{tif_file_name}.nc'
-        expected_normal_path = save_dir / f'preds_normal_{tif_file_name}.nc'
+    @patch('src.models.Model')
+    @patch('scripts.predict.plot_results')
+    def test_make_prediction_forecasted(self, mock_plot_results, mock_model):
+        mock_model.predict.return_value = xr.Dataset(None)
+        output_path = make_prediction(model=mock_model,
+                                      test_path=self.empty_tif,
+                                      save_dir=self.temp_dir,
+                                      with_forecaster=True,
+                                      plot_results_enabled=True)
+        mock_model.predict.assert_called()
+        mock_plot_results.assert_called()
+        self.assertEqual(output_path, self.forecasted_path)
+        self.assertTrue(output_path.exists())
+        self.forecasted_path.unlink()
 
-        for p in [expected_forecasted_path, expected_normal_path]:
-            if p.exists():
-                p.unlink()
-
-        # Test making forecasted prediction
-        output_file_path = make_prediction(model=mock_model,
-                                      test_path=test_path,
-                                      save_dir=save_dir,
-                                      with_forecaster=True)
-        self.assertEqual(output_file_path, expected_forecasted_path)
-        self.assertTrue(output_file_path.exists())
-
-        # Test that prediction is not made if file already exists
-        output_file_path = make_prediction(model=mock_model,
-                                           test_path=test_path,
-                                           save_dir=save_dir,
-                                           with_forecaster=True)
-        self.assertEqual(output_file_path, None)
-
-        # Test making normal prediction
-        output_file_path = make_prediction(model=mock_model,
-                                           test_path=test_path,
-                                           save_dir=save_dir,
-                                           with_forecaster=False)
-        self.assertEqual(output_file_path, expected_normal_path)
-        self.assertTrue(output_file_path.exists())
-
+    @patch('src.models.Model')
+    @patch('scripts.predict.plot_results')
+    def test_make_prediction_already_exists(self, mock_plot_results, mock_model):
+        self.normal_path.touch()
+        output_path = make_prediction(model=mock_model,
+                                      test_path=self.empty_tif,
+                                      save_dir=self.temp_dir)
+        mock_model.predict.assert_not_called()
+        mock_plot_results.assert_not_called()
+        self.assertEqual(output_path, None)
+        self.normal_path.unlink()
 
     def test_gdal_merge(self):
+        # Figuring out if gdal_merge should have an argument for forecasted vs normal
         print('hello')
