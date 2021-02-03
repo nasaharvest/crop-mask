56f09b713bcf537924a98fe7a0187f750025cd54
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-02-03 08:10:26 -0800

    touch empty aws credentials if not available

--- train.sh
+++ train.sh
@@ -3,38 +3,36 @@ export DOCKER_BUILDKIT=1
 # Builds docker image with landcover-mapping conda environment to avoid reinstalling dependencies often
 docker build -f Dockerfile.deps -t cropmask/dependencies .
 
-printf "Once model begins training, you can view progress here:\n\
-https://app.community.clear.ml/projects/15e0af16e2954760b5acf7d0117d4cdc\n"
-
-
-export AWS_CREDENTIALS="" #$HOME/.aws/credentials
-export CLEARML_CREDENTIALS=$HOME/clearml.conf
 export DATASETS="geowiki_landcover_2017,kenya_non_crop,one_acre_fund_kenya,plant_village_kenya"
 export MODEL_NAME=kenya
+export AWS_CREDENTIALS=$HOME/.aws/credentials
+export CLEARML_CREDENTIALS=$HOME/clearml.conf
 
-aws_build_secret () {
-  if test -f "$1"
+pull_credentials_from_secrets () {
+  if test ! -f "$1"
     then
-      echo "--secret id=aws,src=$1"
-    else
-      echo ""
+      aws secretsmanager get-secret-value --secret-id $2 --region us-east-1  --query SecretString --output text > $1
   fi
 }
 
-aws_build_secret_mount () {
-  if test -f "$1"
+check_aws_credentials () {
+  if test ! -f "$AWS_CREDENTIALS"
     then
-      echo "--mount=type=secret,id=aws,target=/root/.aws/credentials"
-    else
-      echo ""
+      touch "$AWS_CREDENTIALS"
   fi
 }
+
+pull_credentials_from_secrets "$CLEARML_CREDENTIALS" "ivan/clearml.conf"
+check_aws_credentials
+
+printf "Once model begins training, you can view progress here:\n\
+https://app.community.clear.ml/projects/15e0af16e2954760b5acf7d0117d4cdc\n"
 
 # Trains model 
 docker build -f Dockerfile.train \
 -t cropmask/train-$MODEL_NAME \
---secret id=clearml,src=$CLEARML_CREDENTIALS $(aws_build_secret "$AWS_CREDENTIALS") \
---build-arg AWS_BUILD_SECRET_MOUNT=$(aws_build_secret_mount "$AWS_CREDENTIALS") \
+--secret id=aws,src=$AWS_CREDENTIALS \
+--secret id=clearml,src=$CLEARML_CREDENTIALS \
 --build-arg DATASETS=$DATASETS \
 --build-arg MODEL_NAME=$MODEL_NAME \
 --output data \
