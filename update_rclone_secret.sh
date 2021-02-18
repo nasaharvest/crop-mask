8462ee0f0602419efb81f17ce32cc6b767318e21
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-02-17 20:26:25 -0800

    More detail

--- update_rclone_secret.sh
+++ update_rclone_secret.sh
@@ -1,3 +1,7 @@
+# This script simplifies the process of updated the rclone secret on AWS.
+# The rclone credential file expires very often (because of Google Drive) 
+# and thus needs to be constantly updated.
+
 # Checks rclone is working
 rclone version
 
