7c217e74b22ea94f63ca57baa53f87589f796680
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-03-26 13:16:41 -0700

    centralized label processing

--- scripts/process.py
+++ scripts/process.py
@@ -1,41 +1,12 @@
 import logging
 import sys
-from pathlib import Path
 
 sys.path.append("..")
 
-from src.processors import (
-    GeoWikiProcessor,
-    KenyaPVProcessor,
-    KenyaNonCropProcessor,
-    KenyaOAFProcessor,
-)
+from src.dataset_config import datasets
 
 logging.basicConfig(level=logging.INFO)
 
-
-def process_geowiki():
-    processor = GeoWikiProcessor(Path("../data"))
-    processor.process()
-
-
-def process_plantvillage():
-    processor = KenyaPVProcessor(Path("../data"))
-    processor.process()
-
-
-def process_kenya_noncrop():
-    processor = KenyaNonCropProcessor(Path("../data"))
-    processor.process()
-
-
-def process_one_acre():
-    processor = KenyaOAFProcessor(Path("../data"))
-    processor.process()
-
-
 if __name__ == "__main__":
-    process_geowiki()
-    process_plantvillage()
-    process_kenya_noncrop()
-    process_one_acre()
+    for d in datasets:
+        d.process_labels()
--- src/dataset_config.py
+++ src/dataset_config.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from src.ETL.dataset import Dataset
+from src.ETL.dataset import Dataset, Processor
 
 
 class DatasetName(Enum):
@@ -7,6 +7,13 @@ class DatasetName(Enum):
     KenyaNonCrop = "kenya_non_crop"
     KenyaOAF = "one_acre_fund_kenya"
     KenyaPV = "plant_village_kenya"
+
+
+def geowiki_file_name(participants: str = "all") -> str:
+    participants_to_file_labels = {"all": "all", "students": "con", "experts": "exp"}
+    file_label = participants_to_file_labels.get(participants, participants)
+    assert file_label in participants_to_file_labels.values(), f"Unknown participant {file_label}"
+    return f"loc_{file_label}{'_2' if file_label == 'all' else ''}.txt"
 
 
 datasets = [
@@ -18,22 +25,36 @@ datasets = [
         val_set_size=0.2,
         test_set_size=0,
         is_global=True,
+        processors=(Processor(file_name=geowiki_file_name(), custom_geowiki_processing=True),),
     ),
     Dataset(
         dataset=DatasetName.KenyaNonCrop.value,
         sentinel_dataset="earth_engine_kenya_non_crop",
         crop_probability=0.0,
+        processors=(
+            Processor(file_name="noncrop_labels_v2", lat_lon_transform=False),
+            Processor(file_name="noncrop_labels_set2", lat_lon_transform=False),
+            Processor(file_name="2019_gepro_noncrop", x_y_reversed=True, lat_lon_transform=True),
+            Processor(
+                file_name="noncrop_water_kenya_gt", x_y_reversed=True, lat_lon_transform=True
+            ),
+            Processor(file_name="noncrop_kenya_gt", x_y_reversed=True, lat_lon_transform=True),
+        ),
     ),
     Dataset(
         dataset=DatasetName.KenyaOAF.value,
         sentinel_dataset="earth_engine_one_acre_fund_kenya",
         crop_probability=1.0,
         is_maize=True,
+        processors=(Processor(file_name="", x_y_from_centroid=False, lat_lon_lowercase=True),),
     ),
     Dataset(
         dataset=DatasetName.KenyaPV.value,
         sentinel_dataset="earth_engine_plant_village_kenya",
         crop_probability=1.0,
         crop_type_func=lambda overlap: overlap.crop_type,
+        processors=(
+            Processor(file_name="field_boundaries_pv_04282020.shp", lat_lon_transform=True),
+        ),
     ),
 ]
