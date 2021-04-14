c5daa3ba9b9f877f89f5dd96db388b9c725a1ff9
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-04-14 06:57:47 -0700

    Reconfigure test script to be based on model folder

--- scripts/test.py
+++ scripts/test.py
@@ -1,39 +1,23 @@
 import pytorch_lightning as pl
 from pathlib import Path
-from argparse import ArgumentParser
 
 from src.models import Model
 
 
-def get_checkpoint(version: int) -> str:
-
-    log_folder = (
+def get_model_path(name: str) -> str:
+    model_path = (
         Path(__file__).parent.parent
         / "data"
-        / "lightning_logs"
-        / f"version_{version}"
-        / "checkpoints"
+        / "models"
+        / f"{name}.ckpt"
     )
-    checkpoint = list(log_folder.glob("*.ckpt"))
-    return str(checkpoint[0])
-
-
-def test_model():
-    parser = ArgumentParser()
-
-    parser.add_argument("--version", type=int, default=0)
-
-    args = parser.parse_args()
-
-    model_path = get_checkpoint(args.version)
-
-    print(f"Using model {model_path}")
-
-    model = Model.load_from_checkpoint(model_path)
-
-    trainer = pl.Trainer()
-    trainer.test(model)
+    return str(model_path)
 
 
 if __name__ == "__main__":
-    test_model()
+    name = "mali"
+    print(f"Using model {name}")
+    model_path = get_model_path(name)
+    model = Model.load_from_checkpoint(model_path)
+    trainer = pl.Trainer()
+    trainer.test(model)
