dd6ce56a2f542700e23ab3bbd727cfff9b493ecc
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-04-20 10:01:35 -0700

    Add diagrams into readme

--- scripts/process.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-from src.dataset_config import labeled_datasets
-
-logging.basicConfig(level=logging.INFO)
-
-if __name__ == "__main__":
-    for d in labeled_datasets:
-        d.process_labels()
