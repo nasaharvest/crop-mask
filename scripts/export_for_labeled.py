d3e7a6e7b5399e1014b1f5b8a65947dc37a70d4d
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-04-29 06:05:35 -0700

    Fix flake8

--- scripts/export_for_labeled.py
+++ scripts/export_for_labeled.py
@@ -3,7 +3,6 @@ Downloads labels if available, processed labels to have common format,
 and exports datasets using Google Earth Engine
 (locally, or to Google Drive)
 """
-import logging
 import sys
 
 sys.path.append("..")
@@ -15,5 +14,3 @@ if __name__ == "__main__":
         d.download_raw_labels()
         d.process_labels()
         d.export_earth_engine_data()
-
-
--- src/ETL/data_instance.py
+++ src/ETL/data_instance.py
@@ -1,6 +1,6 @@
 import numpy as np
 from dataclasses import dataclass
-from typing import Optional, Union
+from typing import Union
 from .ee_boundingbox import BoundingBox
 
 
