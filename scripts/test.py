b2430baa743d14f3b2207200120ac32eab8d83be
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-04-30 14:20:05 -0700

    Generate model metrics

--- scripts/test.py
+++ scripts/test.py
@@ -1,18 +1,23 @@
 import pytorch_lightning as pl
+import json
 from pathlib import Path
 
 from src.models import Model
 
-
-def get_model_path(name: str) -> str:
-    model_path = Path(__file__).parent.parent / "data" / "models" / f"{name}.ckpt"
-    return str(model_path)
-
-
 if __name__ == "__main__":
-    name = "togo-refeature-all"
-    print(f"Using model {name}")
-    model_path = get_model_path(name)
-    model = Model.load_from_checkpoint(model_path)
-    trainer = pl.Trainer()
-    trainer.test(model)
+    data_dir = Path(__file__).parent.parent / "data"
+    model_metrics_path = data_dir / "model_metrics.json"
+    with model_metrics_path.open() as f:
+        model_metrics = json.load(f)
+
+    for model_path in (data_dir / "models").iterdir():
+        if model_path.suffix != ".ckpt" or model_path.stem in model_metrics:
+            continue
+
+        model = Model.load_from_checkpoint(str(model_path))
+        trainer = pl.Trainer()
+        trainer.test(model)
+        model_metrics[model_path.stem] = {k: round(float(v), 4) for k, v in trainer.callback_metrics.items()}
+
+    with model_metrics_path.open("w") as f:
+        json.dump(model_metrics, f, ensure_ascii=False, indent=4)
