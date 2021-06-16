caf16a2bd1f2bf3504f130a30885c6de9465e0e3
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-06-16 08:38:38 -0700

    Be more strict about what is a duplicate

--- scripts/engineer.py
+++ scripts/engineer.py
@@ -10,7 +10,7 @@ from data.datasets_labeled import labeled_datasets  # noqa: E402
 logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
-    dataset_name = "earth_engine_rwanda"
+    dataset_name = "earth_engine_uganda"
     for d in labeled_datasets:
         if d.sentinel_dataset == dataset_name:
             d.create_pickled_labeled_dataset()
