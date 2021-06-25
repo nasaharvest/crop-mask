b5e96a2379a78ed64a17ee03ce426cd8799dc974
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-06-25 12:03:32 -0700

    Update status and merging code

--- gcp/merger/main.py
+++ gcp/merger/main.py
@@ -1,21 +1,31 @@
 from glob import glob
+from typing import Optional
 import os
+
+build_full_vrt = False
+
+
+def gdal_cmd(cmd_type: str, in_file: str, out_file: str, msg: Optional[str] = None):
+    if cmd_type == "gdalbuildvrt":
+        cmd = f"gdalbuildvrt {out_file} {in_file}"
+    elif cmd_type == "gdal_translate":
+        cmd = f"gdal_translate -a_srs EPSG:4326 -of GTiff {in_file} {out_file}"
+    else:
+        raise NotImplementedError(f"{cmd_type} not implemented.")
+    if msg:
+        print(msg)
+    print(cmd)
+    os.system(cmd)
+
 
 if __name__ == "__main__":
     p = "/Users/izvonkov/nasaharvest/Uganda2"
 
     print("Building vrt for each batch")
     for i, d in enumerate(glob(p + "/*/")):
-        cmd = f"gdalbuildvrt {p}/{i}.vrt {d}*"
-        print(cmd)
-        os.system(cmd)
+        #gdal_cmd(cmd_type="gdalbuildvrt", in_file=f"{d}*", out_file=f"{p}/{i}.vrt")
+        gdal_cmd(cmd_type="gdal_translate", in_file=f"{p}/{i}.vrt", out_file=f"{p}/{i}.tif")
 
-    print("Building full vrt")
-    cmd = f"gdalbuildvrt {p}/final.vrt {p}/*.vrt"
-    print(cmd)
-    os.system(cmd)
-
-    print("Vrt to tif")
-    cmd = f"gdal_translate -of GTiff {p}/final.vrt {p}/final.tif"
-    print(cmd)
-    os.system(cmd)
+    if build_full_vrt:
+        gdal_cmd(cmd_type="gdalbuildvrt", in_file=f"{p}/*.vrt", out_file=f"{p}/final.vrt", msg="Building full vrt")
+        gdal_cmd(cmd_type="gdal_translate", in_file=f"{p}/final.vrt", out_file=f"{p}/final.tif", msg="Vrt to tif")
