1aaac681fe900e0f1c36e1b97133d2f50c4997a8
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-08-04 15:31:51 -0400

    Retrain models

--- scripts/generate_metrics.py
+++ scripts/generate_metrics.py
@@ -1,4 +1,4 @@
 from src.generate_metrics import get_metrics_for_all_models
 
 if __name__ == "__main__":
-    get_metrics_for_all_models(test_mode=True)
+    get_metrics_for_all_models(test_mode=False)
