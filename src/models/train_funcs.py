47bdf19dc1f003e2d4e785255e1984b5fb5e1d3e
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-08-14 12:25:24 -0700

    organize hparam logging

--- src/models/train_funcs.py
+++ src/models/train_funcs.py
@@ -34,14 +34,19 @@ def train_model(model: pl.LightningModule, hparams: Namespace) -> pl.LightningMo
     global_train_dataset = model.get_dataset("training", is_global_only=True)
     local_val_dataset = model.get_dataset("validation", is_local_only=True, upsample=False)
 
-    hparams.local_train_crop_percentage = local_train_dataset.crop_percentage
-    hparams.global_train_crop_percentage = global_train_dataset.crop_percentage
-    hparams.local_val_crop_percentage = local_val_dataset.crop_percentage
+    # local train
     hparams.local_train_original_size = local_train_dataset.original_size
     hparams.local_train_upsampled_size = len(local_train_dataset)
+    hparams.local_train_crop_percentage = local_train_dataset.crop_percentage
+
+    # global train
     hparams.global_train_original_size = global_train_dataset.original_size
     hparams.global_train_upsampled_size = len(global_train_dataset)
-    hparams.local_val_original_size = len(local_val_dataset)
+    hparams.global_train_crop_percentage = global_train_dataset.crop_percentage
+
+    # local val
+    hparams.local_val_size = len(local_val_dataset)
+    hparams.local_val_crop_percentage = local_val_dataset.crop_percentage
 
     logger.experiment.add_hparams(vars(hparams), trainer.callback_metrics)
 
