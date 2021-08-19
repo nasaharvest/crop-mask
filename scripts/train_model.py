78b577560053f94159a1120e3d63eb60dc207190
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-08-19 11:13:52 -0700

    Add model dir

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -33,6 +33,7 @@ if __name__ == "__main__":
     parser.add_argument("--max_epochs", type=int, default=1000)
     parser.add_argument("--patience", type=int, default=10)
     parser.add_argument("--data_folder", type=str, default=data_folder)
+    parser.add_argument("--model_dir", type=str, default=data_folder + "/models")
 
     model_args = Model.add_model_specific_args(parser).parse_args()
     model = Model(model_args)
