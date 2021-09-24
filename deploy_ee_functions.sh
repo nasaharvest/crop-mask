7c41fd44b2b8cff054e48930090e2c1f47341620
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-09-24 10:30:33 -0400

    Add bbox to function

--- deploy_ee_functions.sh
+++ deploy_ee_functions.sh
@@ -1,6 +1,7 @@
 export BUCKET=crop-mask-earthengine
 
 cp -R src/ETL gcp/export_unlabeled_function/src
+cp -R src/bounding_boxes.py gcp/export_unlabeled_function/src
 
 gcloud functions deploy export-unlabeled \
     --source=gcp/export_unlabeled_function \
