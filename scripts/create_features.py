950c0ebd4ca516d233d541c37327d4e216618315
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-10-28 01:15:15 -0400

    Rewrite as create features

--- scripts/create_features.py
+++ scripts/create_features.py
@@ -4,16 +4,17 @@ and exports datasets using Google Earth Engine
 (locally, or to Google Drive)
 """
 import os
+import pandas as pd
 import sys
+
 
 # Change the working directory to the directory of this script
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
 sys.path.append("..")
+
 from src.datasets_labeled import labeled_datasets  # noqa: E402
 
 if __name__ == "__main__":
     for d in labeled_datasets:
-        d.download_raw_labels()
-        d.process_labels()
-        d.export_earth_engine_data()
+        d.create_features()
