93a8c5e1413473789e77078d4bbd462d0a7af179
ivanzvonkov <ivan.zvonkov@gmail.com>
2021-12-13 15:33:24 -0500

    Make integration test whole evaluation process

--- test/integration_test_model_evaluation.py
+++ test/integration_test_model_evaluation.py
@@ -1,22 +1,37 @@
 from sklearn.metrics import f1_score
 from tqdm import tqdm
 from unittest import TestCase
+import json
 import torch
 import pytorch_lightning as pl
 
 from src.models.model import Model
-from src.utils import get_dvc_dir
+from src.utils import get_dvc_dir, metrics_file
 
 
 class IntegrationTestModelEvaluation(TestCase):
-    def test_model_eval(self):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.scores = []
+
         model_dir = get_dvc_dir("models")
-        model_names = [m.stem for m in model_dir.glob("*.ckpt")]
-        no_differences = True
+        with metrics_file.open("r") as f:
+            model_metrics = json.load(f)
 
-        scores = []
+        for model_name, metric_dict in tqdm(model_metrics.items()):
+            if "encoded_val_local_f1_score" in metric_dict:
+                key = "encoded_val_local_f1_score"
+            elif "unencoded_val_local_f1_score" in metric_dict:
+                key = "unencoded_val_local_f1_score"
+            else:
+                raise ValueError(f"No F1 score in {metric_dict}")
 
-        for model_name in model_names:
+            recorded_f1 = metric_dict[key]
+
+            if not (model_dir / f"{model_name}.ckpt").exists():
+                cls.scores.append((model_name, recorded_f1, None, None, None))
+                continue
+
             model_ckpt = Model.load_from_checkpoint(model_dir / f"{model_name}.ckpt")
             model_ckpt.eval()
 
@@ -27,6 +42,7 @@ class IntegrationTestModelEvaluation(TestCase):
                 upsample=False,
                 cache=False,
             )
+
             # Get tensors from validation set
             x = torch.stack([v[0] for v in val])
             y_true = torch.stack([v[1] for v in val])
@@ -40,91 +56,47 @@ class IntegrationTestModelEvaluation(TestCase):
             ckpt_f1 = round(f1_score(y_true=y_true, y_pred=y_pred_ckpt_binary), 4)
 
             trainer = pl.Trainer(checkpoint_callback=False, logger=False)
-
             trainer.model = model_ckpt
             trainer.main_progress_bar = tqdm(disable=True)
             trainer.run_evaluation(test_mode=False)
 
-            # F1 score check
-            if "encoded_val_local_f1_score" in trainer.callback_metrics:
-                key = "encoded_val_local_f1_score"
-            elif "unencoded_val_local_f1_score" in trainer.callback_metrics:
-                key = "unencoded_val_local_f1_score"
-            else:
-                raise ValueError("No F1 score in trainer callback metrics")
-
             trainer_f1 = round(trainer.callback_metrics[key], 4)
 
-            scores.append((model_name, ckpt_f1, trainer_f1))
+            if not (model_dir / f"{model_name}.pt").exists():
+                cls.scores.append((model_name, recorded_f1, ckpt_f1, trainer_f1, None))
+                continue
 
-        for model_name, ckpt_f1, trainer_f1 in scores:
+            model_pt = torch.jit.load(model_dir / f"{model_name}.pt")
+            model_pt.eval()
+
+            with torch.no_grad():
+                y_pred_pt = model_pt(x)[1].numpy()
+
+            y_pred_pt_binary = y_pred_pt > 0.5
+            pt_f1 = round(f1_score(y_true=y_true, y_pred=y_pred_pt_binary), 4)
+
+            cls.scores.append((model_name, recorded_f1, ckpt_f1, trainer_f1, pt_f1))
+
+    def test_model_eval(self):
+        no_differences = True
+        for model_name, recorded_f1, ckpt_f1, trainer_f1, pt_f1 in self.scores:
             print("---------------------------------------------")
             print(model_name)
+            if recorded_f1 == ckpt_f1:
+                print(f"\u2714 Recorded F1 == CKPT F1 == {ckpt_f1}")
+            else:
+                no_differences = False
+                print(f"\u2716 Recorded F1: {recorded_f1} != CKPT F1 {ckpt_f1}")
             if ckpt_f1 == trainer_f1:
                 print(f"\u2714 CKPT F1 == trainer F1 == {trainer_f1}")
             else:
                 no_differences = False
                 print(f"\u2716 CKPT F1: {ckpt_f1} != trainer F1 {trainer_f1}")
+            if pt_f1:
+                if ckpt_f1 == pt_f1:
+                    print(f"\u2714 CKPT F1 == PT F1 == {pt_f1}")
+                else:
+                    no_differences = False
+                    print(f"\u2716 CKPT F1: {ckpt_f1} != PT F1 {pt_f1}")
 
         self.assertTrue(no_differences, "Some ckpt models don't match, check logs.")
-
-    def test_model_pt(self):
-        model_dir = get_dvc_dir("models")
-        model_names = [m.stem for m in model_dir.glob("*.pt")]
-        no_differences = True
-
-        for model_name in model_names:
-            model_ckpt = Model.load_from_checkpoint(model_dir / f"{model_name}.ckpt")
-            model_ckpt.eval()
-            model_pt = torch.jit.load(model_dir / f"{model_name}.pt")
-            model_pt.eval()
-
-            # Get validation set
-            val = model_ckpt.get_dataset(
-                subset="validation",
-                normalizing_dict=model_ckpt.normalizing_dict,
-                upsample=False,
-                cache=False,
-            )
-            # Get tensors from validation set
-            x = torch.stack([v[0] for v in val])
-            y_true = torch.stack([v[1] for v in val])
-
-            # Feed tensors into both models
-            with torch.no_grad():
-                y_pred_ckpt = model_ckpt(x)[1].numpy()
-                y_pred_pt = model_pt(x)[1].numpy()
-
-            y_pred_ckpt_binary = y_pred_ckpt > 0.5
-            y_pred_pt_binary = y_pred_pt > 0.5
-
-            ckpt_f1 = round(f1_score(y_true=y_true, y_pred=y_pred_ckpt_binary), 4)
-            pt_f1 = round(f1_score(y_true=y_true, y_pred=y_pred_pt_binary), 4)
-
-            print("---------------------------------------------")
-            print(model_name)
-
-            # F1 score check
-            if ckpt_f1 == pt_f1:
-                print(f"\u2714 CKPT F1 == PT F1 == {pt_f1}")
-            else:
-                no_differences = False
-                print(f"\u2716 CKPT F1: {ckpt_f1} != PT F1 {pt_f1}")
-
-            if (y_pred_ckpt_binary == y_pred_pt_binary).all():
-                print("\u2714 CKPT binary preds == PT binary preds")
-            else:
-                no_differences = False
-                total = len(y_pred_ckpt_binary)
-                diff = len(y_pred_ckpt_binary[y_pred_ckpt_binary != y_pred_pt_binary])
-                print(f"\u2716 {diff}/{total} binary predictions don't match")
-
-            if (y_pred_ckpt == y_pred_pt).all():
-                print("\u2714 CKPT preds == PT preds")
-            else:
-                no_differences = False
-                total = len(y_pred_ckpt)
-                diff = len(y_pred_ckpt[y_pred_ckpt != y_pred_pt])
-                print(f"\u2716 {diff}/{total} predictions don't match")
-
-        self.assertTrue(no_differences, "Some ckpt and pt models don't match, check logs.")
