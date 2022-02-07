ee1e63f02a6d8e27fe687604b348713bcdc2e97e
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-02-07 17:47:47 -0500

    Use cropharvest's exporter, engineer and move static functions out of class

--- src/ETL/dataset.py
+++ src/ETL/dataset.py
@@ -1,16 +1,26 @@
+from cropharvest.eo import EarthEngineExporter
+from cropharvest.engineer import Engineer
+from datetime import datetime
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, Tuple
+from typing import Dict, List, Tuple
 from tqdm import tqdm
 import logging
 import pandas as pd
+import pickle
 import numpy as np
 
-from .engineer import Engineer
 from .processor import Processor
-from .ee_exporter import LabelExporter
-from src.utils import data_dir, tifs_dir, features_dir, memoize
-from src.ETL.ee_boundingbox import BoundingBox
+from src.utils import (
+    data_dir,
+    tifs_dir,
+    features_dir,
+    memoize,
+    distance,
+    distance_point_from_center,
+    find_nearest,
+)
+from src.ETL.boundingbox import BoundingBox
 from src.ETL.constants import (
     ALREADY_EXISTS,
     COUNTRY,
@@ -28,11 +38,15 @@ from src.ETL.constants import (
     DATASET,
     TIF_PATHS,
 )
+from src.ETL.data_instance import CropDataInstance
 
 logger = logging.getLogger(__name__)
 
 unexported_file = data_dir / "unexported.txt"
 unexported = pd.read_csv(unexported_file, sep="\n", header=None)[0].tolist()
+
+missing_data_file = data_dir / "missing_data.txt"
+missing_data = pd.read_csv(missing_data_file, sep="\n", header=None)[0].tolist()
 
 
 @memoize
@@ -43,6 +57,124 @@ def generate_bbox_from_paths() -> Dict[Path, BoundingBox]:
     }
 
 
+def get_tif_paths(path_to_bbox, lat, lon, start_date, end_date, pbar):
+    candidate_paths = []
+    for p, bbox in path_to_bbox.items():
+        if bbox.contains(lat, lon) and f"dates={start_date}_{end_date}" in p.stem:
+            candidate_paths.append(p)
+    pbar.update(1)
+    return candidate_paths
+
+
+def match_labels_to_tifs(labels: pd.DataFrame) -> pd.Series:
+    bbox_for_labels = BoundingBox(
+        min_lon=labels[LON].min(),
+        min_lat=labels[LAT].min(),
+        max_lon=labels[LON].max(),
+        max_lat=labels[LAT].max(),
+    )
+    # Get all tif paths and bboxes
+    path_to_bbox = {
+        p: bbox for p, bbox in generate_bbox_from_paths().items() if bbox_for_labels.overlaps(bbox)
+    }
+
+    # Match labels to tif files
+    # Faster than going through bboxes
+    with tqdm(total=len(labels), desc="Matching labels to tif paths") as pbar:
+        tif_paths = np.vectorize(get_tif_paths, otypes=[np.ndarray])(
+            path_to_bbox,
+            labels[LAT],
+            labels[LON],
+            labels[START],
+            labels[END],
+            pbar,
+        )
+    return tif_paths
+
+
+def find_matching_point(
+    start: str, tif_paths: List[Path], label_lon: float, label_lat: float
+) -> Tuple[np.ndarray, float, float, str]:
+    """
+    Given a label coordinate (y) this functions finds the associated satellite data (X)
+    by looking through one or multiple tif files.
+    Each tif file contains satellite data for a grid of coordinates.
+    So the function finds the closest grid coordinate to the label coordinate.
+    Additional value is given to a grid coordinate that is close to the center of the tif.
+    """
+    start_date = datetime.strptime(start, "%Y-%m-%d")
+    tif_slope_tuples = [Engineer.load_tif(p, start_date=start_date) for p in tif_paths]
+    if len(tif_slope_tuples) > 1:
+        min_distance_from_point = np.inf
+        min_distance_from_center = np.inf
+        for i, tif_slope_tuple in enumerate(tif_slope_tuples):
+            tif, slope = tif_slope_tuple
+            lon, lon_idx = find_nearest(tif.x, label_lon)
+            lat, lat_idx = find_nearest(tif.y, label_lat)
+            distance_from_point = distance(label_lat, label_lon, lat, lon)
+            distance_from_center = distance_point_from_center(lat_idx, lon_idx, tif)
+            if (distance_from_point < min_distance_from_point) or (
+                distance_from_point == min_distance_from_point
+                and distance_from_center < min_distance_from_center
+            ):
+                closest_lon = lon
+                closest_lat = lat
+                min_distance_from_center = distance_from_center
+                min_distance_from_point = distance_from_point
+                labelled_np = tif.sel(x=lon).sel(y=lat).values
+                average_slope = slope
+                source_file = tif_paths[i].name
+    else:
+        tif, slope = tif_slope_tuples[0]
+        closest_lon = find_nearest(tif.x, label_lon)[0]
+        closest_lat = find_nearest(tif.y, label_lat)[0]
+        labelled_np = tif.sel(x=closest_lon).sel(y=closest_lat).values
+        average_slope = slope
+        source_file = tif_paths[0].name
+
+    labelled_np = Engineer.calculate_ndvi(labelled_np)
+    labelled_np = Engineer.remove_bands(labelled_np)
+    labelled_np = Engineer.fillna(labelled_np, average_slope)
+
+    return labelled_np, closest_lon, closest_lat, source_file
+
+
+def create_pickled_labeled_dataset(labels):
+    for label in tqdm(labels.to_dict(orient="records"), desc="Creating pickled instances"):
+        (labelled_array, tif_lon, tif_lat, tif_file) = find_matching_point(
+            start=label[START],
+            tif_paths=label[TIF_PATHS],
+            label_lon=label[LON],
+            label_lat=label[LAT],
+        )
+
+        instance = CropDataInstance(
+            labelled_array=labelled_array,
+            instance_lat=tif_lat,
+            instance_lon=tif_lon,
+            source_file=tif_file,
+        )
+        save_path = Path(label[FEATURE_PATH])
+        save_path.parent.mkdir(exist_ok=True)
+        with save_path.open("wb") as f:
+            pickle.dump(instance, f)
+
+
+def do_label_and_feature_amounts_match(labels: pd.DataFrame):
+    all_subsets_correct_size = True
+    if not labels[ALREADY_EXISTS].all():
+        labels[ALREADY_EXISTS] = np.vectorize(lambda p: Path(p).exists())(labels[FEATURE_PATH])
+    train_val_test_counts = labels[SUBSET].value_counts()
+    for subset, labels_in_subset in train_val_test_counts.items():
+        features_in_subset = labels[labels[SUBSET] == subset][ALREADY_EXISTS].sum()
+        if labels_in_subset != features_in_subset:
+            print(f"\u2716 {subset}: {labels_in_subset} labels, but {features_in_subset} features")
+            all_subsets_correct_size = False
+        else:
+            print(f"\u2714 {subset} amount: {labels_in_subset}")
+    return all_subsets_correct_size
+
+
 @dataclass
 class LabeledDataset:
     dataset: str = ""
@@ -50,15 +182,10 @@ class LabeledDataset:
 
     # Process parameters
     processors: Tuple[Processor, ...] = ()
-    days_per_timestep: int = 30
 
     def __post_init__(self):
         self.raw_labels_dir = data_dir / "raw" / self.dataset
         self.labels_path = data_dir / "processed" / (self.dataset + ".csv")
-
-    @staticmethod
-    def merge_sources(sources):
-        return ",".join(sources.unique())
 
     def process_labels(self):
         df = pd.DataFrame({})
@@ -69,7 +196,7 @@ class LabeledDataset:
 
         # Go through processors and create new labels if necessary
         new_labels = [
-            p.process(self.raw_labels_dir, self.days_per_timestep)
+            p.process(self.raw_labels_dir)
             for p in self.processors
             if p.filename not in str(already_processed)
         ]
@@ -83,7 +210,7 @@ class LabeledDataset:
         df[NUM_LABELERS] = 1
         df = df.groupby([LON, LAT, START, END], as_index=False, sort=False).agg(
             {
-                SOURCE: self.merge_sources,
+                SOURCE: lambda sources: ",".join(sources.unique()),
                 CROP_PROB: "mean",
                 NUM_LABELERS: "sum",
                 SUBSET: "first",
@@ -108,62 +235,6 @@ class LabeledDataset:
         df.to_csv(self.labels_path, index=False)
         return df
 
-    @staticmethod
-    def get_tif_paths(path_to_bbox, lat, lon, start_date, end_date, pbar):
-        candidate_paths = []
-        for p, bbox in path_to_bbox.items():
-            if bbox.contains(lat, lon) and p.stem.endswith(f"dates={start_date}_{end_date}"):
-                candidate_paths.append(p)
-        pbar.update(1)
-        return candidate_paths
-
-    def do_label_and_feature_amounts_match(self, labels: pd.DataFrame):
-        all_subsets_correct_size = True
-        if not labels[ALREADY_EXISTS].all():
-            labels[ALREADY_EXISTS] = np.vectorize(lambda p: Path(p).exists())(labels[FEATURE_PATH])
-        train_val_test_counts = labels[SUBSET].value_counts()
-        for subset, labels_in_subset in train_val_test_counts.items():
-            features_in_subset = labels[labels[SUBSET] == subset][ALREADY_EXISTS].sum()
-            if labels_in_subset != features_in_subset:
-                print(
-                    f"\u2716 {subset}: {labels_in_subset} labels, but {features_in_subset} features"
-                )
-                all_subsets_correct_size = False
-            else:
-                print(f"\u2714 {subset} amount: {labels_in_subset}")
-        return all_subsets_correct_size
-
-    def generate_feature_paths(self, labels: pd.DataFrame) -> pd.Series:
-        labels["feature_dir"] = str(features_dir)
-        return labels["feature_dir"] + "/" + labels["filename"] + ".pkl"
-
-    def match_labels_to_tifs(self, labels: pd.DataFrame) -> pd.Series:
-        bbox_for_labels = BoundingBox(
-            min_lon=labels[LON].min(),
-            min_lat=labels[LAT].min(),
-            max_lon=labels[LON].max(),
-            max_lat=labels[LAT].max(),
-        )
-        # Get all tif paths and bboxes
-        path_to_bbox = {
-            p: bbox
-            for p, bbox in generate_bbox_from_paths().items()
-            if bbox_for_labels.overlaps(bbox)
-        }
-
-        # Match labels to tif files
-        # Faster than going through bboxes
-        with tqdm(total=len(labels), desc="Matching labels to tif paths") as pbar:
-            tif_paths = np.vectorize(self.get_tif_paths, otypes=[np.ndarray])(
-                path_to_bbox,
-                labels[LAT],
-                labels[LON],
-                labels[START],
-                labels[END],
-                pbar,
-            )
-        return tif_paths
-
     def load_labels(
         self, allow_processing: bool = False, fail_if_missing_features: bool = False
     ) -> pd.DataFrame:
@@ -174,7 +245,9 @@ class LabeledDataset:
         else:
             raise FileNotFoundError(f"{self.labels_path} does not exist")
         labels = labels[labels[CROP_PROB] != 0.5]
-        labels = labels[~labels[FEATURE_FILENAME].isin(unexported)]
+        unexported_labels = labels[FEATURE_FILENAME].isin(unexported)
+        missing_data_labels = labels[FEATURE_FILENAME].isin(missing_data)
+        labels = labels[~unexported_labels & ~missing_data_labels].copy()
         labels["feature_dir"] = str(features_dir)
         labels[FEATURE_PATH] = labels["feature_dir"] + "/" + labels[FEATURE_FILENAME] + ".pkl"
         labels[ALREADY_EXISTS] = np.vectorize(lambda p: Path(p).exists())(labels[FEATURE_PATH])
@@ -211,30 +284,37 @@ class LabeledDataset:
         # -------------------------------------------------
         labels_with_no_features = labels[~labels[ALREADY_EXISTS]].copy()
         if len(labels_with_no_features) == 0:
-            self.do_label_and_feature_amounts_match(labels)
+            do_label_and_feature_amounts_match(labels)
             return
 
         # -------------------------------------------------
         # STEP 3: Match labels to tif files (X)
         # -------------------------------------------------
-        labels_with_no_features[TIF_PATHS] = self.match_labels_to_tifs(labels_with_no_features)
+        labels_with_no_features[TIF_PATHS] = match_labels_to_tifs(labels_with_no_features)
         tifs_found = labels_with_no_features[TIF_PATHS].str.len() > 0
 
-        labels_with_no_tifs = labels_with_no_features.loc[~tifs_found]
+        labels_with_no_tifs = labels_with_no_features.loc[~tifs_found].copy()
         labels_with_tifs_but_no_features = labels_with_no_features.loc[tifs_found]
 
         # -------------------------------------------------
         # STEP 4: If no matching tif, download it
         # -------------------------------------------------
         if len(labels_with_no_tifs) > 0:
-            print(f"{len(labels_with_no_tifs)} labels not matched")
+            print(f"{len(labels_with_no_tifs )} labels not matched")
             if not disable_gee_export:
-                LabelExporter().export(labels=labels_with_no_tifs)
+                labels_with_no_tifs[START] = pd.to_datetime(labels_with_no_tifs[START]).dt.date
+                labels_with_no_tifs[END] = pd.to_datetime(labels_with_no_tifs[END]).dt.date
+                EarthEngineExporter(
+                    labels=labels_with_no_tifs,
+                    check_ee=True,
+                    check_gcp=True,
+                    dest_bucket="crop-mask-tifs2",
+                ).export_for_labels()
 
         # -------------------------------------------------
         # STEP 5: Create the features (X, y)
         # -------------------------------------------------
         if len(labels_with_tifs_but_no_features) > 0:
-            Engineer().create_pickled_labeled_dataset(labels=labels_with_tifs_but_no_features)
+            create_pickled_labeled_dataset(labels=labels_with_tifs_but_no_features)
 
-        self.do_label_and_feature_amounts_match(labels)
+        do_label_and_feature_amounts_match(labels)
