2d85f71095f92e3100701fe234ddfc5928038011
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-02-19 14:39:42 -0500

    Simple script to demonstrate inference

---
+++ scripts/model_inference.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+
+from src.inference import Inference
+from src.models.model import Model
+from src.utils import models_dir
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument("--model_name", type=str)
+    parser.add_argument("--tif_path", type=str)
+    parser.add_argument("--dest_path", type=str)
+
+    args = parser.parse_args()
+    model = Model.load_from_checkpoint(models_dir / f"{args.model_name}.ckpt")
+    Inference(model).run(local_path=args.tif_path, dest_path=args.dest_path)
+
+    
