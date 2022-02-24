ef6b8b3eb97a327ac6826da00ff69b09e763f0eb
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-02-23 21:31:47 -0500

    Manually set bbox

--- scripts/model_train.py
+++ scripts/model_train.py
@@ -2,9 +2,7 @@
 Script that uses argument parameters to train an individual model
 """
 import os
-import geopandas as gpd
 import sys
-import cartopy.io.shapereader as shpreader
 from argparse import ArgumentParser
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
@@ -19,13 +17,6 @@ all_datasets_str = ",".join(
 )
 
 if __name__ == "__main__":
-
-    # ne_shapefile = shpreader.natural_earth(
-    #     resolution="10m", category="cultural", name="admin_0_countries"
-    # )
-    # ne_gdf = gpd.read_file(ne_shapefile)
-    # geom = ne_gdf[ne_gdf["ADMIN"] == "Rwanda"].iloc[0].geometry
-    # min_lon, min_lat, max_lon, max_lat = geom.bounds
 
     train_datasets_str = (
         "geowiki_landcover_2017,Kenya,Mali,Mali_lower_CEO_2019,Mali_upper_CEO_2019,"
