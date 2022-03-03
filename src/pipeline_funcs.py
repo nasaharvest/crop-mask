764cb376d2cdbdc7ac8359b16dda5eb68e82d7d3
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-03-02 19:22:40 -0500

    Another flake8 thing

--- src/pipeline_funcs.py
+++ src/pipeline_funcs.py
@@ -36,7 +36,7 @@ def validate(hparams: Namespace) -> Namespace:
     return hparams
 
 
-def train_model(hparams, offline: bool = False) -> Tuple[pl.LightningModule, Dict[str, float]]:
+def train_model(hparams, offline: bool = False) -> Tuple[pl.LightningModule, Dict[str, Dict[str, Any]]]:
 
     early_stop_callback = EarlyStopping(
         monitor="val_loss",
