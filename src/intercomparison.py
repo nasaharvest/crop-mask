545e8a1b5c85b166d8d0ab0eb97f1692ecd0ab1c
Adam Yang <adamfyang@gmail.com>
2022-07-11 13:24:58 -0400

    Added doc string and remove list return param.

--- src/intercomparison.py
+++ src/intercomparison.py
@@ -1,4 +1,5 @@
-import sklearn.metrics
+from sklearn.metrics import classification_report, accuracy_score, f1_score
+from src.ETL.constants import CROP_PROB, SUBSET
 import geopandas as gpd
 import pandas as pd
 import rasterio as rio
@@ -8,30 +9,40 @@ import numpy as np
 import json
 import os
 
-def run_comparison(validationPath: str, cropmapPath: str, validation_projection = None, cropmap_projection: str = None, asJSON = False, subset: str = None):
+def run_comparison(validation_path: str, cropmap_path: str, validation_projection: str = None, cropmap_projection: str = None, subset: str = None) -> dict:
+  """ 
+  Returns a dictionary of intercomparison metrics. 
+
+  Arguments:
+  validation_path - filepath for validation csv
+  cropmap_path - filepath for cropmap tiff file
+  validaiton_projeciton -  espg projection of csv
+  crop_map_projection - espg projection of tiff
+  subset - subset of csv used for validation
+
+  """
     
   # Assume datatypes to be csv and tiff for validation and cropmap respectively
-  report = []
-  cropmap = rio.open(cropmapPath)
-  validation = gpd.read_file(validationPath)
+  report = {}
+  cropmap = rio.open(cropmap_path)
+  validation = gpd.read_file(validation_path)
 
   # Resets csv to specified subset
   if subset != None:
-    validation = validation[validation['subset'] == subset]
+    validation = validation[validation[SUBSET] == subset]
 
-  validation['crop_probability'] = validation['crop_probability'].astype(float)
+  validation[CROP_PROB] = validation[CROP_PROB].astype(float)
 
   # Map values to binary values
-  if np.unique(validation['crop_probability']).size != 2:
-    validation.loc[validation['crop_probability'].astype(int) >= 0.5, 'crop_probability'] = 1
-    validation.loc[validation['crop_probability'].astype(int) < 0.5, 'crop_probability'] = 0
+  if np.unique(validation[CROP_PROB]).size != 2:
+    validation.loc[validation[CROP_PROB].astype(int) >= 0.5, CROP_PROB] = 1
+    validation.loc[validation[CROP_PROB].astype(int) < 0.5, CROP_PROB] = 0
   
-
   pIn = Proj(init='epsg:4326')
   out = Proj(init=cropmap.crs.to_dict()['init'])
 
   # Reproject csv to tif projection
-  validation = gpd.GeoDataFrame(data = validation[['lat', 'lon', 'crop_probability']])
+  validation = gpd.GeoDataFrame(data = validation[['lat', 'lon', CROP_PROB]])
   newLat, newLon = transform(pIn, out, np.array(validation['lon']), np.array(validation['lat']))
 
   # Sample points from tif files
@@ -40,7 +51,7 @@ def run_comparison(validationPath: str, cropmapPath: str, validation_projection 
 
   # Adds sampled data to dataframe
   combined_maps = pd.DataFrame()
-  combined_maps['validation'] = validation['crop_probability']
+  combined_maps['validation'] = validation[CROP_PROB]
   combined_maps['cropmap'] = cropmap_sampled
 
   if np.unique(combined_maps['cropmap']).size != 2:
@@ -48,20 +59,18 @@ def run_comparison(validationPath: str, cropmapPath: str, validation_projection 
 
   # Calculate metrics
   target_names = ['non_crop', 'crop']
-  class_report = sklearn.metrics.classification_report(combined_maps['validation'], combined_maps['cropmap'], target_names=target_names, output_dict=True)
-  accuracy = sklearn.metrics.accuracy_score(combined_maps['validation'], combined_maps['cropmap'])
-  f1 = sklearn.metrics.f1_score(combined_maps['validation'], combined_maps['cropmap'])
+  class_report = classification_report(combined_maps['validation'], combined_maps['cropmap'], target_names=target_names, output_dict=True)
+  accuracy = accuracy_score(combined_maps['validation'], combined_maps['cropmap'])
+  f1 = f1_score(combined_maps['validation'], combined_maps['cropmap'])
 
-  if asJSON:
-    report = {
-        'dataset' : cropmapPath.split('/')[-1],
-        'f1' : f1,
-        'crop precision' : class_report['crop']['precision'], 
-        'crop recall' : class_report['crop']['recall'],
-        'non-crop precision' : class_report['non_crop']['precision'], 
-        'non-crop recall' : class_report['non_crop']['recall']
-    }
-  else: 
-    report = [f1, accuracy, class_report['crop']['precision'], class_report['non_crop']['precision'], class_report['non_crop']['recall'], class_report['crop']['recall']]
-
+  report = {
+      'dataset' : cropmap_path
+    .split('/')[-1],
+      'f1' : f1,
+      'crop precision' : class_report['crop']['precision'], 
+      'crop recall' : class_report['crop']['recall'],
+      'non-crop precision' : class_report['non_crop']['precision'], 
+      'non-crop recall' : class_report['non_crop']['recall']
+  }
+    
   return report
