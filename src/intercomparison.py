dfba58f1817d91329c773714913aa49543c9015d
Adam Yang <adamfyang@gmail.com>
2022-08-18 12:59:02 -0400

    reformatted function and added link to crop maps

--- src/intercomparison.py
+++ src/intercomparison.py
@@ -1,4 +1,4 @@
-from sklearn.metrics import classification_report, accuracy_score, f1_score
+from sklearn.metrics import classification_report, accuracy_score
 from src.ETL.constants import CROP_PROB, SUBSET, LAT, LON
 import geopandas as gpd
 import pandas as pd
@@ -7,68 +7,80 @@ from pyproj import Proj
 from pyproj import transform
 import numpy as np
 
-def run_comparison(validation_path: str, cropmap_path: str, validation_projection: str = None, cropmap_projection: str = None, subset: str = None) -> dict:
-  """ 
-  Returns a dictionary of intercomparison metrics. 
 
-  Arguments:
-  validation_path - filepath for validation csv
-  cropmap_path - filepath for cropmap tiff file
-  validaiton_projeciton -  espg projection of csv
-  crop_map_projection - espg projection of tiff
-  subset - subset of csv used for validation
+def run_comparison(
+    validation_path: str,
+    cropmap_path: str,
+    validation_projection: str = None,
+    cropmap_projection: str = None,
+    subset: str = None,
+) -> dict:
+    """
+    Returns a dictionary of intercomparison metrics.
 
-  """
-    
-  # Assume datatypes to be csv and tiff for validation and cropmap respectively
-  report = {}
-  cropmap = rio.open(cropmap_path)
-  validation = gpd.read_file(validation_path)
+    Arguments:
+    validation_path - filepath for validation csv
+    cropmap_path - filepath for cropmap tiff file
+    validaiton_projeciton -  espg projection of csv
+    crop_map_projection - espg projection of tiff
+    subset - subset of csv used for validation
 
-  # Resets csv to specified subset
-  if subset != None:
-    validation = validation[validation[SUBSET] == subset]
+    """
 
-  validation[CROP_PROB] = validation[CROP_PROB].astype(float)
+    # Assume datatypes to be csv and tiff for validation and cropmap respectively
+    report = {}
+    cropmap = rio.open(cropmap_path)
+    validation = gpd.read_file(validation_path)
 
-  # Map values to binary values
-  if np.unique(validation[CROP_PROB]).size != 2:
-    validation.loc[validation[CROP_PROB] >= 0.5, CROP_PROB] = 1
-    validation.loc[validation[CROP_PROB] < 0.5, CROP_PROB] = 0
-  
-  pIn = Proj(init='epsg:4326')
-  out = Proj(init=cropmap.crs.to_dict()['init'])
+    # Resets csv to specified subset
+    if subset is not None:
+        validation = validation[validation[SUBSET] == subset]
 
-  # Reproject csv to tif projection
-  validation = gpd.GeoDataFrame(data = validation[[LAT, LON, CROP_PROB]])
-  newLat, newLon = transform(pIn, out, np.array(validation[LON]), np.array(validation[LAT]))
+    validation[CROP_PROB] = validation[CROP_PROB].astype(float)
 
-  # Sample points from tif files
-  cropmap_sampled = rio.sample.sample_gen(cropmap, zip(newLat, newLon))
-  cropmap_sampled = np.array([x for x in cropmap_sampled]).astype(int)
+    # Map values to binary values
+    if np.unique(validation[CROP_PROB]).size != 2:
+        validation.loc[validation[CROP_PROB] >= 0.5, CROP_PROB] = 1
+        validation.loc[validation[CROP_PROB] < 0.5, CROP_PROB] = 0
 
-  # Adds sampled data to dataframe
-  combined_maps = pd.DataFrame()
-  combined_maps['validation'] = validation[CROP_PROB]
-  combined_maps['cropmap'] = cropmap_sampled
+    pIn = Proj(init="epsg:4326")
+    out = Proj(init=cropmap.crs.to_dict()["init"])
 
-  if np.unique(combined_maps['cropmap']).size != 2:
-    combined_maps.loc[combined_maps['cropmap'] >= 0.5, 'cropmap'] = 1
-    combined_maps.loc[combined_maps['cropmap'] < 0.5, 'cropmap'] = 0
+    # Reproject csv to tif projection
+    validation = gpd.GeoDataFrame(data=validation[[LAT, LON, CROP_PROB]])
+    newLat, newLon = transform(pIn, out, np.array(validation[LON]), np.array(validation[LAT]))
 
-  # Calculate metrics
-  target_names = ['non_crop', 'crop']
-  class_report = classification_report(combined_maps['validation'], combined_maps['cropmap'], target_names=target_names, output_dict=True)
-  accuracy = accuracy_score(combined_maps['validation'], combined_maps['cropmap'])
+    # Sample points from tif files
+    cropmap_sampled = rio.sample.sample_gen(cropmap, zip(newLat, newLon))
+    cropmap_sampled = np.array([x for x in cropmap_sampled]).astype(int)
 
-  report = {
-      'dataset' : cropmap_path.split('/')[-1],
-      'accuracy' : accuracy,
-      'f1' : class_report['crop']['f1-score'],
-      'crop precision' : class_report['crop']['precision'], 
-      'crop recall' : class_report['crop']['recall'],
-      'non-crop precision' : class_report['non_crop']['precision'], 
-      'non-crop recall' : class_report['non_crop']['recall']
-  }
-    
-  return report
+    # Adds sampled data to dataframe
+    combined_maps = pd.DataFrame()
+    combined_maps["validation"] = validation[CROP_PROB]
+    combined_maps["cropmap"] = cropmap_sampled
+
+    if np.unique(combined_maps["cropmap"]).size != 2:
+        combined_maps.loc[combined_maps["cropmap"] >= 0.5, "cropmap"] = 1
+        combined_maps.loc[combined_maps["cropmap"] < 0.5, "cropmap"] = 0
+
+    # Calculate metrics
+    target_names = ["non_crop", "crop"]
+    class_report = classification_report(
+        combined_maps["validation"],
+        combined_maps["cropmap"],
+        target_names=target_names,
+        output_dict=True,
+    )
+    accuracy = accuracy_score(combined_maps["validation"], combined_maps["cropmap"])
+
+    report = {
+        "dataset": cropmap_path.split("/")[-1],
+        "accuracy": accuracy,
+        "f1": class_report["crop"]["f1-score"],
+        "crop precision": class_report["crop"]["precision"],
+        "crop recall": class_report["crop"]["recall"],
+        "non-crop precision": class_report["non_crop"]["precision"],
+        "non-crop recall": class_report["non_crop"]["recall"],
+    }
+
+    return report
