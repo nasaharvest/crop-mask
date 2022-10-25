99af5e9fd4cb5cd1bcaf77609681d9f27343ef75
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-10-25 13:50:09 -0400

    Deploy using inputs

--- src/bboxes.py
+++ src/bboxes.py
@@ -14,5 +14,4 @@ bboxes = {
     "Togo": BBox(min_lat=6.089, max_lat=11.134, min_lon=-0.1501, max_lon=1.778),
     "Uganda": BBox(min_lat=-1.63, max_lat=4.30, min_lon=29.12, max_lon=35.18),
     "Tanzania": BBox(min_lat=-13.794, max_lat=0.801, min_lon=28.082, max_lon=41.331),
-    "Zambia": BBox(min_lat=-18.334, max_lat=-7.536, min_lon=21.379, max_lon=33.991),
 }
