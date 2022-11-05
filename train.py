cb48aafa11914302a0ffceec8dd190f4ee8d2963
ivanzvonkov <ivan.zvonkov@gmail.com>
2022-11-05 18:20:33 -0400

    Small default train dataset

--- train.py
+++ train.py
@@ -11,11 +11,12 @@ from src.models import Model
 from src.pipeline_funcs import train_model
 
 selected_bbox = bboxes["Sudan_Blue_Nile"]
+train_datasets = [datasets[1].dataset]  # [d.dataset for d in datasets]
 
 parser = ArgumentParser()
 parser.add_argument("--model_name", type=str, default="Sudan_Blue_Nile_2019")
 parser.add_argument("--eval_datasets", type=str, default="Sudan_Blue_Nile_CEO_2019")
-parser.add_argument("--train_datasets", type=str, default=",".join([d.dataset for d in datasets]))
+parser.add_argument("--train_datasets", type=str, default=",".join(train_datasets))
 parser.add_argument("--min_lat", type=float, default=selected_bbox.min_lat)
 parser.add_argument("--max_lat", type=float, default=selected_bbox.max_lat)
 parser.add_argument("--min_lon", type=float, default=selected_bbox.min_lon)
