1784b543b46dc40a16a0045168b6d035ed4e953a
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2024-01-30 15:42:40 +0800

    Improve tests; varying kept indices per batch

--- test/test_presto.py
+++ test/test_presto.py
@@ -37,11 +37,14 @@ class PrestoTest(unittest.TestCase):
         num_timesteps = 12
         x = torch.tensor([-1, 0, 0, 1, 2, 2, 2, 3, 4, 5, 6, 7, 8]).float()
         x = repeat(x, "t -> b t d", b=2, d=128)
-        kept_indices = [0, 1, 12, 24, 25, 26, 36, 48, 60, 72, 84, 85]
-        kept_indices_t = torch.tensor(kept_indices)
-        kept_indices_t = torch.stack((kept_indices_t, kept_indices_t))
+        kept_indices = torch.tensor(
+            [
+                [0, 1, 12, 24, 25, 26, 36, 48, 60, 72, 84, 85],
+                [0, 8, 12, 24, 25, 28, 36, 48, 60, 72, 84, 85],
+            ]
+        )
         model = Presto.construct()
-        out = model.encoder.band_groups_mean(x, kept_indices_t, num_timesteps)
+        out = model.encoder.band_groups_mean(x, kept_indices, num_timesteps)
         expected_out = torch.arange(0, len(BANDS_GROUPS_IDX))
         expected_out = torch.repeat_interleave(expected_out, 128)
         expected_out = torch.stack((expected_out, expected_out))
