f7de3187c6fc105891cdcf7ba70f6dfbc2440f05
Adebowale Daniel <dansy37@gmail.com>
2024-06-10 12:32:27 -0400

    Created new dataset

--- datasets.py
+++ datasets.py
@@ -539,6 +539,36 @@ class UgandaNorthCEO2019(LabeledDataset):
         df[SUBSET] = train_val_test_split(df.index, 0.3, 0.3)
         return df
 
+class KenyaCropArea2019(LabeledDataset):
+    def load_labels(self) -> pd.DataFrame:
+        raw_folder = raw_dir / "Kenya_Crop_Area_2019"
+        df1 = pd.read_csv(
+            raw_folder
+            / "ceo-Kenya-Crop-Area-Estimation-Reference-Sample-2019---Set-1-sample-data-2024-06-10.csv"
+        )
+        df2 = pd.read_csv(
+            raw_folder
+            / "ceo-Kenya-Crop-Area-Estimation-Reference-Sample-2019---Set-2-sample-data-2024-06-10.csv"
+        )
+        df =pd.concat([df1,df2])
+
+        # Discard rows with no label
+        df = df[~df["Does this point correspond to active cropland?"].isna()].copy()
+        df[CLASS_PROB] = df["Does this point correspond to active cropland?"] == "Crop"
+        df[CLASS_PROB] = df[CLASS_PROB].astype(int)
+        df["num_labelers"] = 1
+        df = df.groupby([LON, LAT], as_index=False, sort=False).agg(
+            {
+                CLASS_PROB: "mean",
+                "num_labelers": "sum",
+                "plotid": join_unique,
+                "sampleid": join_unique,
+                "email": join_unique,
+            }
+        )
+        df[START], df[END] = date(2019, 1, 1), date(2020, 12, 31)
+        df[SUBSET] = train_val_test_split(df.index, 0.3, 0.3)
+        return df
 
 datasets: List[LabeledDataset] = [
     CustomLabeledDataset(
@@ -1327,6 +1357,7 @@ datasets: List[LabeledDataset] = [
     Uganda_NorthCEO2022(),
     Uganda_NorthCEO2021(),
     UgandaNorthCEO2019(),
+    KenyaCropArea2019(),
 ]
 
 if __name__ == "__main__":
